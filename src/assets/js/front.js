dd83a80347a4cfc78f2270440f32783b36a687ac
kik8727 <kik87827@gmail.com>
2023-11-25 07:39:37 +0900

    수정작업완료

--- src/assets/js/front.js
+++ src/assets/js/front.js
@@ -38,13 +38,7 @@ function commonInit() {
     document.querySelector("html").classList.add(opt);
   }
   
-  // 퍼블확인용
-  let url = window.location.href.split('?')[1];
-  let url_2 = url !== undefined ? url.split('#')[0] : null;
-
-  if(url_2 === "dark"){
-      document.querySelector("html").classList.add("dark_theme");
-  }
+  
 }
 
 /*
